8e997a9f53770b45fb55117a2e0d4c07699d0b16
Gabriel Portela <gab.portela@hotmail.com>
2016-11-11 15:14:02 -0200

    created the Game structure

--- main.c
+++ main.c
@@ -10,18 +10,34 @@
 	#define clear system("clear")
 #endif
 
-void draw_stickman(char stickman[][22], int miss);
+typedef struct{
+	char secretWord[8];
+	char knownWord[8];
+	char usedLetters[26];
+	int mistakes;
+	int tries;
+	char stickman[7][22];
+} Game;
+
+void draw_stickman(Game *game);
 
 int main(void)
-{
-	char stickman[7][22] = {
-				{ "   ________         "  },
-				{ "   |      |         "  },
-				{ "   |                "  },
-				{ "   |                "  },
-				{ "   |                "  },
-				{ "   |                "  },
-				{ "--------------------"  }	
+{	
+	Game game = {
+		"",	// secretWord
+		"",	// knownWord
+		"",	// usedLetters
+		0,	// mistakes
+		0,	// tries
+		{ 
+			{ "   ________         "  },
+			{ "   |      |         "  },
+			{ "   |                "  },
+			{ "   |                "  },
+			{ "   |                "  },
+			{ "   |                "  },
+			{ "--------------------"  }
+		}
 	};
 	
 	/* Should make the program read theses
@@ -35,14 +51,14 @@ int main(void)
 		"apple"
 	};
 	
-	char secretWord[20];
-	memset( secretWord, '\0', sizeof(secretWord) );
+	//char secretWord[20];
+	memset( game.secretWord, '\0', sizeof(game.secretWord) );
 	
-	char knownWord[20];
-	memset( knownWord, '\0', sizeof(knownWord) );
+	//char knownWord[20];
+	memset( game.knownWord, '\0', sizeof(game.knownWord) );
 	
-	char usedLetters[26];
-	memset( usedLetters, '\0', sizeof(usedLetters) );
+	//char usedLetters[26];
+	memset( game.usedLetters, '\0', sizeof(game.usedLetters) );
 	
 	int mistakes=0;
 	int right;
@@ -73,7 +89,7 @@ int main(void)
 	
 				int w = rand()%5;
 	
-				strcpy( secretWord, &words[w][8] );
+				strcpy( game.secretWord, &words[w][8] );
 			
 				play=1;
 				menu = 0;
@@ -81,10 +97,10 @@ int main(void)
 		
 			case 2:
 				printf("\nType word: ");
-				fgets(secretWord, sizeof(secretWord), stdin);
+				fgets(game.secretWord, sizeof(game.secretWord), stdin);
 				
-				if (secretWord[strlen(secretWord)-1] == '\n')
-					secretWord[strlen(secretWord)-1] = '\0';
+				if (game.secretWord[strlen(game.secretWord)-1] == '\n')
+					game.secretWord[strlen(game.secretWord)-1] = '\0';
 			
 				play=1;
 				menu = 0;
@@ -102,15 +118,15 @@ int main(void)
 			
 			clear;
 			
-			draw_stickman(stickman, mistakes);
+			draw_stickman(&game);
 			
 			printf("\n\t");
 			
-			for (int i=0; i < strlen(secretWord); ++i) {
-				if (knownWord[i] == '\0') {
+			for (int i=0; i < strlen(game.secretWord); ++i) {
+				if (game.knownWord[i] == '\0') {
 					putchar('_');
 				} else {
-					printf( "%c", knownWord[i] );
+					printf( "%c", game.knownWord[i] );
 				}
 			}
 			
@@ -118,18 +134,18 @@ int main(void)
 			putchar('\n');
 			
 			printf("Used Letters:\n\t");
-			for (int i=0; ( i < strlen(usedLetters) ) && (usedLetters[i] != '\0'); ++i ) {
+			for (int i=0; ( i < strlen(game.usedLetters) ) && (game.usedLetters[i] != '\0'); ++i ) {
 				inWord = 0;
 				
-				for (int j=0; j < sizeof(knownWord); ++j) {
-					if (usedLetters[i] == knownWord[j]) {
+				for (int j=0; j < sizeof(game.knownWord); ++j) {
+					if (game.usedLetters[i] == game.knownWord[j]) {
 						inWord = 1;
 						break;
 					}
 				}
 				
 				if (!inWord) {
-					printf("%c ", usedLetters[i]);
+					printf("%c ", game.usedLetters[i]);
 				}
 			}
 			putchar('\n');
@@ -138,7 +154,7 @@ int main(void)
 			
 			if (mistakes == 6) {
 				printf("Game Over...\n");
-				printf("The word was: %s\n", secretWord);
+				printf("The word was: %s\n", game.secretWord);
 				printf("Continue?(s/n) ");
 				scanf("%c", &c);
 				getchar();
@@ -146,20 +162,18 @@ int main(void)
 				if (c == 's') {
 					mistakes = 0;
 					tries = 0;
-				
-					/* Reset stickman */
-					stickman[2][10] = ' ';			
-					stickman[3][10] = ' ';
-					stickman[3][ 9] = ' ';
-					stickman[3][11] = ' ';
-					stickman[4][ 9] = ' ';
-					stickman[4][11] = ' ';
 					
-					/* Reset variables */
-					memset( secretWord, '\0', sizeof(secretWord) );
-					memset( knownWord, '\0', sizeof(knownWord) );
-					memset( usedLetters, '\0', sizeof(usedLetters) );
-				
+					game.stickman[2][10] = ' ';			
+					game.stickman[3][10] = ' ';
+					game.stickman[3][ 9] = ' ';
+					game.stickman[3][11] = ' ';
+					game.stickman[4][ 9] = ' ';
+					game.stickman[4][11] = ' ';
+					
+					memset( game.secretWord, '\0', sizeof(game.secretWord) );
+					memset( game.knownWord, '\0', sizeof(game.knownWord) );
+					memset( game.usedLetters, '\0', sizeof(game.usedLetters) );
+					
 					menu=1;
 					play=0;
 					break;
@@ -167,7 +181,7 @@ int main(void)
 					clear;
 					break;
 				}
-			} else if ( strcmp(secretWord, knownWord) == 0 ) {
+			} else if ( strcmp(game.secretWord, game.knownWord) == 0 ) {
 				printf("\nWon the game!\n");
 				
 				printf("\nContinue?(s/n) ");
@@ -178,18 +192,16 @@ int main(void)
 					mistakes = 0;
 					tries = 0;
 					
-					/* Reset stickman */
-					stickman[2][10] = ' ';
-					stickman[3][10] = ' ';
-					stickman[3][ 9] = ' ';
-					stickman[3][11] = ' ';
-					stickman[4][ 9] = ' ';
-					stickman[4][11] = ' ';
+					game.stickman[2][10] = ' ';
+					game.stickman[3][10] = ' ';
+					game.stickman[3][ 9] = ' ';
+					game.stickman[3][11] = ' ';
+					game.stickman[4][ 9] = ' ';
+					game.stickman[4][11] = ' ';
 					
-					/* Reset variables */
-					memset( secretWord, '\0', sizeof(secretWord) );
-					memset( knownWord, '\0', sizeof(knownWord) );
-					memset( usedLetters, '\0', sizeof(usedLetters) );
+					memset( game.secretWord, '\0', sizeof(game.secretWord) );
+					memset( game.knownWord, '\0', sizeof(game.knownWord) );
+					memset( game.usedLetters, '\0', sizeof(game.usedLetters) );
 					
 					menu=1;
 					play=0;
@@ -215,7 +227,7 @@ int main(void)
 			c = tolower(c);
 			
 			for ( int i=0; i < 26; ++i) {
-				if ( usedLetters[i] == c ) {
+				if ( game.usedLetters[i] == c ) {
 					printf("Already guessed that letter!");
 				
 					getchar();
@@ -224,15 +236,15 @@ int main(void)
 				}
 			}
 			
-			usedLetters[tries] = c;
+			game.usedLetters[tries] = c;
 			++tries;
 			
 			right = 0;
 			
 			/* Check for mistakes */
-			for ( int pos=0; pos < strlen(secretWord); ++pos ) {
-				if( c == secretWord[pos] ) {
-					knownWord[pos] = c;
+			for ( int pos=0; pos < strlen(game.secretWord); ++pos ) {
+				if( c == game.secretWord[pos] ) {
+					game.knownWord[pos] = c;
 					right = 1;
 				}
 			}
@@ -250,40 +262,45 @@ int main(void)
 	return 0;
 }
 
-void draw_stickman(char stickman[][22], int mistakes)
+void draw_stickman(Game *game)
 {
 	/* Add parts to the stickman */
-	switch(mistakes) {
+	switch(game->mistakes) {
 		case 1:
-			stickman[2][10] = 'O';
+			game->stickman[2][10] = 'O';
 		break;
 		
 		case 2:
-			stickman[3][10] = '|';
+			game->stickman[3][10] = '|';
 		break;
 		
 		case 3:
-			stickman[3][9] = '/';
+			game->stickman[3][9] = '/';
 		break;
 		
 		case 4:
-			stickman[3][11] = '\\';
+			game->stickman[3][11] = '\\';
 		break;
 		
 		case 5:
-			stickman[4][9] = '/';
+			game->stickman[4][9] = '/';
 		break;
 		
 		case 6:
-			stickman[4][11] = '\\';
+			game->stickman[4][11] = '\\';
 		break;
 	}
 	
 	/* Actually draw the picture */
 	for (int i=0; i<7; ++i) {
 		for (int j=0; j<22; ++j) {
-			putchar(stickman[i][j]);
+			putchar(game->stickman[i][j]);
 		}
 		putchar('\n');
 	}
+}
+
+void reset_game(Game *game)
+{
+
 }
